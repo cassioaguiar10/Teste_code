57aa715091808740579a80305e192efca519f628
cassioaguiar10 <cassio.aguiar@yahoo.com.br>
2025-07-22 19:24:11 -0300

    new main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 DB_PATH = config.DB_PATH
 
 # funçao para inicializar o banco de dados
-
 def init_db():
     with sqlite3.connect(DB_PATH) as conn:
         cursor = conn.cursor()
@@ -21,30 +20,27 @@ def init_db():
                 inadimplencia REAL             
                 )
         ''')
-
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS selic (
                 mes TEXT PRIMARY KEY,
                 selic_diaria REAL         
                 )
         ''')               
-
         conn.commit()
-# em breve =)
-vazio = 0
         
+# em breve =)
+vazio = 0   
 @app.route('/')
 def index():
     return render_template_string('''
             <h1>Upload de dadods Economicos</h1>
             <form action='/upload' method='POST' enctype='multipart/form-data'>
-                <label for='campo_inadimplencia'>Arquivo de Inadimplencia</label>
-                                  
-            <input name='campo_inadimplencia' type='file' required><br><br>     
+                <label for='campo_inadimplencia'>Arquivo de Inadimplencia</label>                
+                <input name='campo_inadimplencia' type='file' required><br><br>     
 
            <label for='campo_selic'>Arquivo Taxa Selic</label>
-           <input type='file' required name='campo_selic' required><br><br>                      
-             <input type='submit' value='Fazer Upload'>
+           <input type='file' name='campo_selic' required><br><br>                      
+            <input type='submit' value='Fazer Upload'>
             </form> 
             <br><br><hr>
             <a href='/consultar'> Consultar Dados <br>      
@@ -57,11 +53,10 @@ def index():
 def upload():
     inad_file = request.files.get('campo_inadimplencia')
     selic_file = request.files.get('campo_selic')
-
     #verificar se os aquivos de fato foram enviados
     if not inad_file or not selic_file:
         return jsonify({'Erro': 'Amabos os arquivos devem ser enviados '})
-    
+        
     inad_df = pd.read_csv(
         inad_file, 
         sep = ';',
@@ -75,15 +70,12 @@ def upload():
         names=['data', 'selic_diaria'],
         header = 0 
     )    
-
 # formata o campo de dada como daha hora padrao
     inad_df['data'] = pd.to_datetime(inad_df['data'],format="%d/%m/%Y")
     selic_df['data'] = pd.to_datetime(inad_df['data'],format="%d/%m/%Y")
-
 #gera uma coluna nova mes e preenche de acordo com a data
     inad_df['mes'] = inad_df['data'].dt.to_period('M').astype(str)
     selic_df['mes'] = selic_df['data'].dt.to_period('M').astype(str)
-
     #limpa as duplicatas de agrupa conjutos
     inad_mensal = inad_df[['mes','inadimplencia']].drop_duplicates()
     selic_mensal = selic_df.groupby('mes')['selic_diaria'].mean().reset_index()
@@ -98,7 +90,7 @@ def upload():
 def consultar():
     #resultados se a pagina for carregada recebendo post
     if request.method == 'POST':
-        tabela = request.form.get('campo_tablea')
+        tabela = request.form.get('campo_tabela')
         if tabela not in ['inadimplencia','selic']:
             return jsonify({'Error': 'Tabela é invalida'})
         with sqlite3.connect(DB_PATH) as conn:
@@ -109,19 +101,15 @@ def consultar():
     return render_template_string(''' 
         <h1> Consultar tabela </h1>
         <form action='/consultar' method='POST'>
-        <label for='campo_tabela'> Escolha a tabela:  
-        </label>
+        <label for='campo_tabela'> Escolha a tabela: </label>
          <select name='campo_tabela'>
          <option value='inadimplencia'> Inadimplencia </option>
          <option value='selic'> Selic </option>
-         </select>
-                                  
-        <input type='submit' value='Consultar'>
-                                                          
+         </select>                          
+        <input type='submit' value='Consultar'>                                                          
         </form>
         <br>
         <a href='/'>Voltar</a>
-
     ''')
 
 if __name__ == '__main__':
